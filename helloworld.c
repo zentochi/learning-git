1044327e565c404185e1d3436878549f68e9f626
Zentochi <danuarta241@gmail.com>
2023-01-06 14:11:13 +0700

    Edited first message

--- helloworld.c
+++ helloworld.c
@@ -3,7 +3,7 @@
 int main ()
 {
     char user[20];
-    printf("Whats your name?\n") 
+    printf("Please input your name\n") 
     scanf(user);
     printf ("Hello %s!. have a awesome day!\n",&user);
     
